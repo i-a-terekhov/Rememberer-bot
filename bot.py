84d2171e70fe8548a35528325c39a8f451b28ee3
Ivan Terekhov <for-spam32@yandex.ru>
2024-02-29 22:13:47 +0400

    feat: add description
    rememberer-bot

--- bot.py
+++ bot.py
@@ -8,36 +8,10 @@ from hidden.tokenfile import TOKEN_FIVE as TOKEN
 from hidden.tokenfile import OWNER_CHAT_ID as CHAT_ID
 
 from handlers.echo_plug import service_router, regular_router
-from schedule.main_objects import ListOfTasks
+from schedule.main_objects import periodic_start_for_functions
 
 
 bot_unit = Bot(TOKEN)
-tasks = ListOfTasks()
-
-
-#TODO произвести рефактор: все функции работы с расписанием перенести в отдельный модуль. Для работы со всеми
-# объектами и функциями расписания вывести одну функцию
-
-# Для проверки работы periodic_start_for_functions
-async def send_message_for_check(text: str):
-    print('send_message_for_check')
-    await bot_unit.send_message(chat_id=CHAT_ID, text=text)
-
-
-async def check_list_of_tasks():
-    if tasks.check_empty():
-        text = 'Есть некоторые задачи'
-    else:
-        text = 'Нет задач'
-        tasks.add_task('Первая задача!')
-    await send_message_for_check(text=text)
-
-
-async def periodic_start_for_functions():
-    print('periodic_start_for_functions')
-    while True:
-        await check_list_of_tasks()
-        await asyncio.sleep(60 * 5)
 
 
 async def main(bot: Bot):
@@ -63,7 +37,10 @@ async def main(bot: Bot):
 
 
 async def gathering_functions():
-    await asyncio.gather(periodic_start_for_functions(), main(bot=bot_unit))
+    await asyncio.gather(
+        periodic_start_for_functions(bot=bot_unit, chat_id=CHAT_ID),
+        main(bot=bot_unit)
+    )
 
 
 if __name__ == '__main__':
--- schedule/main_objects.py
+++ schedule/main_objects.py
@@ -1,9 +1,14 @@
+import asyncio
+
+from aiogram import Bot
+
 
 class RegularSchedule:
     def __init__(self):
         pass
 
 
+#TODO Сделать описание
 class ListOfTasks:
     def __init__(self):
         self.list_of_tasks = list()
@@ -18,47 +23,37 @@ class ListOfTasks:
         self.list_of_tasks.append(describe)
 
 
-class RoomsTasks:
-    default_list_of_tasks = [
+#TODO Сделать описание
+class Task:
+    def __init__(self, room, text, category, reminder_time):
+        self.room = room  # уник. имя комнаты - ключ в словаре, значения для которого - лист с ID участников комнаты
+        self.text = text  # текст задачи - нулевое значение листа, остальные значения листа - подзадачи
+        self.reminder_time = reminder_time
+
+    def __str__(self):
+        return f"Task: {self.text}, Text: {self.text}, Reminder Time: {self.reminder_time}"
+
+    def make_task(self):
+        default_list_of_tasks = [
             'Не забыть сделать',
             'Не забыть купить',
             'Не забыть посетить',
             'Обновить пароли на сервисы'
         ]
 
-    def __init__(self):
-        self.list_of_tasks = self.default_list_of_tasks
 
-    def add_new_category(self, new_category: str) -> bool:
-        if new_category in self.list_of_tasks:
-            print('такая категория уже существует')
-            return False
-        else:
-            self.list_of_tasks.append(new_category)
-            return True
+#TODO Сделать описание
+# Класс Room создает экземпляры комнат, основной задачей которых является сохранение списка участников, для рассылки им
+# сообщений с задачами и сообщений о поставленных и выполненных задачах
+# Функционал комнат должен подразумевать наличие роли админа (одного или нескольких).
+# Админ решает, кто может добавлять задачи (только админы или все), кто может утвердить задачу
+# выполненной (только админы или все), производится ли рассылка задач всем или только ответственным.
 
-    def del_category(self, category: str) -> None:
-        self.list_of_tasks.remove(category)
-
-    def return_to_default(self) -> None:
-        self.__init__()
-
-
-class Task:
-    def __init__(self, room, text, category, reminder_time):
-        self.room = room  # уник. имя комнаты - ключ в словаре, значения для которого - лист с ID участников комнаты
-        self.text = text  # текст задачи - ключ в словаре, значения для которого - подзадачи
-        self.reminder_time = reminder_time
-
-    def __str__(self):
-        return f"Task: {self.text}, Text: {self.text}, Reminder Time: {self.reminder_time}"
-
-
-class Group:
-    def __init__(self, group_id):
-        self.group_id = group_id
+class Room:
+    def __init__(self, room_name):
+        self.room_name = room_name
         self.members = []
-        self.categories = Categories()
+        self.categories = []
         self.tasks = []
 
     def add_member(self, user_id):
@@ -68,4 +63,34 @@ class Group:
         task = Task(text, category, reminder_time)
         self.tasks.append(task)
         return task
+
+
+tasks = ListOfTasks()
+
+
+# Для проверки работы periodic_start_for_functions
+async def send_message_for_check(bot_unit: Bot, chat_id: str, text: str):
+    print('send_message_for_check')
+    await bot_unit.send_message(chat_id=chat_id, text=text)
+
+
+async def check_list_of_tasks(bot_unit: Bot, chat_id: str):
+    if tasks.check_empty():
+        text = 'Есть некоторые задачи'
+    else:
+        text = 'Нет задач'
+        tasks.add_task('Первая задача!')
+    await send_message_for_check(bot_unit=bot_unit, chat_id=chat_id, text=text)
+
+
+async def periodic_start_for_functions(bot: Bot, chat_id: str):
+    # Функция проходит по всему пулу задач, отправляя каждую в те чаты, которые указаны в каждой таске.
+    # Если задач нет, ничего не происходит.
+    # Задачи появляются по нажатию кнопки "создать задачу" и заполнении данных для нее.
+    # Задачи исчезают по нажатию на одну из кнопок: "удалить задачу" или "задача выполнена" с рассылкой соответсвующего
+    # сообщения всем участникам комнаты
+
+    while True:
+        await check_list_of_tasks(bot_unit=bot, chat_id=chat_id)
+        await asyncio.sleep(60 * 5)
 
