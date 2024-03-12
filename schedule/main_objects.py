77d6b90142d32f76a4a536520e1ae8663c4c559d
Ivan Terekhov <for-spam32@yandex.ru>
2024-03-12 17:11:58 +0400

    feat: make base classes
    rememberer-bot

--- schedule/main_objects.py
+++ schedule/main_objects.py
@@ -1,4 +1,5 @@
 import asyncio
+from random import randint
 
 from aiogram import Bot
 from aiogram.types import Message
@@ -12,66 +13,99 @@ class RegularSchedule:
 
 
 class Users:
+    users = tuple()
+
     def __init__(self, message: Message):
         self.telegram_id = message.from_user.id
         self.nickname = message.from_user.username
+        Users.users = (self.telegram_id, )
+        print('Словарь юзеров: ', Users.users)
 
 
-class Room:
+class Rooms:
     users_and_roles = {}
 
-    def __init__(self, room_name, message: Message):
+    def __init__(self, room_name: str, message: Message):
+        user_id = str(message.from_user.id)
+
         self.name = room_name
-        self.owner = message.from_user.id
-        self.admins = [message.from_user.id]
-        self.members = [message.from_user.id]
+        self.owner = user_id
+        self.admins = [user_id]
+        self.members = [user_id]
         self.password = message.text
         self.rights_to_create_task = 'all_users'
 
-    def add_configurate(self, user_id, room_name):
-        configurate = 'user_id' + '_&_' + 'room_name'
-        self.users_and_roles[configurate] = {
+        configurate = user_id + '_&_' + room_name
+        Rooms.users_and_roles[configurate] = {
                 'telegram_id': user_id,
-                'nickname': self.owner,
+                'nickname': message.from_user.username,
                 'room': room_name,
                 'role': 'admin'
             }
 
-
-class Task:
-    def __init__(self, room_name, text, author, executor):
-        self.number = "random number"  # уник. номер комнаты, будет генерится функцией
-        self.room = room_name
-        self.text = text  # текст задачи
-        self.author = author
-        self.executor = executor
-        self.period_of_remind = '30:00'
-        self.execution_level = 0.0
-        self.accept_by_author = False
-        self.accept_in_time = '2025-12-31 23:59:59'
-        self.livetime_after_ending = '12:00:00'
-        self.list_of_recipients = [author]
-
-    def __str__(self):
-        return f"Task: {self.text}, Text: {self.text}, Accept in Time: {self.accept_in_time}"
-
-
-# Класс ListOfTasks создает экземпляр хранилища всех задач, не различая задачи между комнатами
-class ListOfTasks:
-    def __init__(self):
-        self.list_of_tasks = list()
-
-    def check_empty(self):
-        if len(self.list_of_tasks) == 0:
-            return False
-        else:
+    @classmethod
+    def is_room_exist(cls, room_name: str):
+        if room_name in cls.users_and_roles:
             return True
+        else:
+            return False
 
-    def add_task(self, describe: Task):
-        self.list_of_tasks.append(describe)
+    def add_user_to_room(self, room_name: str, message: Message):
+        configurate = str(message.from_user.id) + '_&_' + room_name
+        Rooms.users_and_roles[configurate] = {
+            'telegram_id': str(message.from_user.id),
+            'nickname': message.from_user.username,
+            'room': room_name,
+            'role': 'user'
+        }
 
 
-tasks = ListOfTasks()
+class Tasks:
+    all_tasks = {}
+
+    def __init__(self, room_name, text, author, executor):
+        # self.number = "random number"  # уник. номер комнаты, будет генерится функцией
+        self.new_task = {
+                'room': room_name,
+                'text': text,
+                'author': author,
+                'executor': executor,
+                'period_of_remind': '30:00',
+                'execution_level': 0.0,
+                'accept_by_author': False,
+                'accept_in_time': '2025-12-31 23:59:59',
+                'livetime_after_ending': '12:00:00',
+                'list_of_recipients': [author],
+        }
+
+    @classmethod
+    def _is_task_number_exist(cls, number: str):
+        if number in cls.all_tasks.keys():
+            return True
+        else:
+            return False
+
+    def _make_unice_number(self) -> str:
+        while True:
+            number = str(randint(1, 1000))
+            if Tasks._is_task_number_exist(number):
+                continue
+            else:
+                return str(number)
+
+    def save_task(self):
+        number = Tasks._make_unice_number(self)
+        Tasks.all_tasks[number] = self.new_task
+
+
+def check_empty():
+    if len(Tasks.all_tasks) > 0:
+        return True
+    else:
+        return False
+
+
+task_01 = Tasks(room_name='01', text='sadfas', author='me', executor='2342')
 
 
 # Для проверки работы periodic_start_for_functions
@@ -81,11 +115,12 @@ async def send_message_for_check(bot_unit: Bot, chat_id: str, text: str):
 
 
 async def check_list_of_tasks(bot_unit: Bot, chat_id: str):
-    if tasks.check_empty():
+
+    if check_empty():
         text = f'{current_time()}: Есть некоторые задачи'
     else:
         text = f'{current_time()}: Нет задач'
-        tasks.add_task('Первая задача!')
+        task_01.save_task()
     await send_message_for_check(bot_unit=bot_unit, chat_id=chat_id, text=text)
 
 
@@ -98,5 +133,5 @@ async def periodic_start_for_functions(bot: Bot, chat_id: str):
 
     while True:
         await check_list_of_tasks(bot_unit=bot, chat_id=chat_id)
-        await asyncio.sleep(60 * 5)
+        await asyncio.sleep(60 * 2)
 
