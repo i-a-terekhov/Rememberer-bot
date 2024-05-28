9e461fe6256103dc043b48cdade15e64d417cb14
Ivan Terekhov <for-spam32@yandex.ru>
2024-05-28 23:47:12 +0400

    feat: add new task (sprint_240528), some tasks finished (look to sprint.txt)
    rememberer-bot

--- schedule/time.py
+++ schedule/time.py
@@ -10,3 +10,18 @@ def current_datatime() -> str:
     curr_time = datetime.now().strftime('%y.%m.%d, %H:%M')
     return curr_time
 
+
+def parse_datetime(date_string: str) -> datetime:
+    """
+    Функция принимает строку с датой и временем в формате 'YYYY-MM-DD HH:MM'
+    и возвращает объект datetime.
+    """
+    return datetime.strptime(date_string, '%Y-%m-%d %H:%M')
+
+
+def datetime_to_string(dt: datetime) -> str:
+    """
+    Функция принимает объект datetime и возвращает строку с датой и временем
+    в формате 'YYYY-MM-DD HH:MM'.
+    """
+    return dt.strftime('%Y-%m-%d %H:%M')
